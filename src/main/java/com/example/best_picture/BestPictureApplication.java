28f2240d76deefd31c4bf4b8fff18bd519b79df8
ryo.fujinaga <ryo.fujinaga@g.softbank.co.jp>
2021-06-07 09:17:18 +0900

    Initial commit

---
+++ src/main/java/com/example/best_picture/BestPictureApplication.java
@@ -0,0 +1,13 @@
+package com.example.best_picture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestPictureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestPictureApplication.class, args);
+	}
+
+}
